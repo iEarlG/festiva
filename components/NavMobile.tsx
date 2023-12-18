7e171a2c59b78824f3397dcab8bef22a79403d10
Earl A. Villapaz <jasonburning16@gmail.com>
2023-12-18 19:36:16 +0800

    0: Seperator

--- components/NavMobile.tsx
+++ components/NavMobile.tsx
@@ -1,8 +1,26 @@
 
+import Image from "next/image";
+
+import { Menu } from "lucide-react";
+
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+  
 export const NavMobile = () => {
     return (
-        <div>
-            NavMobile
-        </div>
+        <nav className="md:hidden">
+            <Sheet>
+                <SheetTrigger className="align-middle">
+                    <Menu className="w-6 h-6 cursor-pointer" />
+                </SheetTrigger>
+                <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+                    <Image 
+                        alt="logo"
+                        src="/logo.svg"
+                        width={128}
+                        height={38}
+                    />
+                </SheetContent>
+            </Sheet>
+        </nav>
     )
 };
