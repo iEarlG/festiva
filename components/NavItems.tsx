a9b6b1abc9ed2ac9a3b84ae3f4bfa93765afd5b9
Earl A. Villapaz <jasonburning16@gmail.com>
2023-12-18 20:00:03 +0800

    2: Navigation

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,8 +1,30 @@
+"use client"
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import { headerLinks } from "@/constants/links";
 
 export const NavItems = () => {
+    const pathname = usePathname();
+
     return (
-        <div>
-            NavItems
-        </div>
+        <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+            {headerLinks.map((link) => {
+                const isActive = pathname === link.route;
+
+                return (
+                    <li
+                        key={link.label}
+                        className={cn("flex-center p-medium-16 whitespace-nowrap",
+                            isActive && "text-[#0C66EE]"
+                        )}
+                    >
+                        <Link href={link.route}>{link.label}</Link>
+                    </li>
+                )
+            })}
+        </ul>
     )
 };
--- components/NavMobile.tsx
+++ components/NavMobile.tsx
@@ -4,6 +4,9 @@ import Image from "next/image";
 import { Menu } from "lucide-react";
 
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
+
+import { NavItems } from "@/components/NavItems";
   
 export const NavMobile = () => {
     return (
@@ -19,6 +22,8 @@ export const NavMobile = () => {
                         width={128}
                         height={38}
                     />
+                    <Separator className="border border-gray-50" />
+                    <NavItems />
                 </SheetContent>
             </Sheet>
         </nav>
