492a69e5feffdb7c1df2e38590a3e0b563ebbedb
Earl A. Villapaz <jasonburning16@gmail.com>
2023-12-19 21:33:11 +0800

    3: Footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
 
 export const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="border-t">
+            <div className="flex-center wrapper flex-between flex-container flex-col gap-4 p-5 text-center sm:flex-row">
+                <Link href="/">
+                    <Image 
+                        alt="Festiva Logo"
+                        src="/logo.svg"
+                        width={128}
+                        height={38}
+                    />
+                </Link>
+                <p><span className="text-[#FF006B]">2023 Festiva.</span> All rights reserved.</p>
+            </div>
+        </footer>
     )
 };
